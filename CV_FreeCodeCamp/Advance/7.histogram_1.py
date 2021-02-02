919d257719101a08ee8bd2dbd46fcfbc5598938d
OmkarPatkar <patkar.omkar2@gmail.com>
2021-02-02 22:29:45 +0530

    freecodecamp OpenCv-python

--- CV_FreeCodeCamp/Advance/7.histogram_1.py
+++ CV_FreeCodeCamp/Advance/7.histogram_1.py
@@ -8,20 +8,20 @@ cv2.imshow('cats', img)
 
 blank = np.zeros(img.shape[:2], dtype = 'uint8')
 
-circle = cv2.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 155, -1)
+mask = cv2.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 155, -1)
 
 # color histogram
-mask1 = cv2.bitwise_and(img, img, mask = circle)
-cv2.imshow("mask1", mask1)
+masked = cv2.bitwise_and(img, img, mask = mask)
+cv2.imshow("mask1", masked)
 
 plt.figure()
-plt.title('histogram 2')
+plt.title('color histogram ')
 plt.xlabel('bins')
 plt.ylabel('# of pixels')
 
 colors = ('b', 'g', 'r')
 for i, col in enumerate(colors):
-    hist = cv2.calcHist([img], [i], None, [256], [0, 256])
+    hist = cv2.calcHist([img], [i], mask, [256], [0, 256])
     plt.plot(hist, color = col)
     plt.xlim([0, 256])
 plt.show()
